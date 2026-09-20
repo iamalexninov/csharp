18585815e5faefc49a26e5bf3ee33800c8c3adb6
Alex Ninov <iamalexgninov@gmail.com>
2026-09-20 17:34:20 +0300

    solve task: Train from arrays-exercise

--- Fundamentals/FundamentalModuleSolutions/FromLeftToRight/obj/Debug/net8.0/FromLeftToRight.AssemblyInfo.cs
+++ Fundamentals/FundamentalModuleSolutions/FromLeftToRight/obj/Debug/net8.0/FromLeftToRight.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FromLeftToRight")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+919246b4722fe252f38c06981ef186056204c17e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ee6813a9711fb663ea6f3647bbd56687ee37b2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("FromLeftToRight")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FromLeftToRight")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Fundamentals/FundamentalModuleSolutions/Train/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Train
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Train[] outputs = new Train[3]
+            {
+                new Train(3),
+                new Train(6),
+                new Train(1),
+            };
+
+            foreach (var item in outputs)
+                item.Sandbox();
+        }
+    }
+
+    public class Train
+    {
+        private int _times;
+
+        public Train(int times) => this._times = times;
+
+        private int[] ReceiveInputsAndFillArr(int times)
+        {
+            int[] arr = new int[times];
+            for (int i = 0; i < times; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                arr[i] = input;
+            }
+
+            return arr;
+        }
+
+        private string PrintElementInArr(int[] arr) => string.Join(" ", arr);
+
+        private int CalculateSum(int[] arr) => arr.Sum();
+
+        public void Sandbox()
+        {
+            int[] arr = this.ReceiveInputsAndFillArr(this._times);
+            Console.WriteLine(this.PrintElementInArr(arr));
+            Console.WriteLine(this.CalculateSum(arr));
+        }
+    }
+}
